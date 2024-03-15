9f1fc97c9c12a4895c7f64014c1884f443384b22
bastianhs <140167233+bastianhs@users.noreply.github.com>
2024-03-15 07:57:04 +0700

    feat: add general bezier curve for n points

--- src/bezier.py
+++ src/bezier.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-# Finding points in quadratic bezier curve with divide and conquer
+# find points in quadratic bezier curve with divide and conquer
 def find_quad_bezier_dnc(
         control_points: np.ndarray[np.ndarray[float]], 
         i: int, 
@@ -15,10 +15,79 @@ def find_quad_bezier_dnc(
     q1: np.ndarray[float] = (p1 + p2) / 2
     r: np.ndarray[float] = (q0 + q1) / 2
 
+    # base case when we reach end of iteration
     if i == num_of_iter:
         return np.array((p0, r, p2))
+    
+    # find 1st half control points
     control_points1: np.ndarray[np.ndarray[float]] = np.array((p0, q0, r))
+
+    # find 2nd half control points
     control_points2: np.ndarray[np.ndarray[float]] = np.array((r, q1, p2))
+    
+    # find bezier curve points from the 1st half and 2nd half
     bezier1: np.ndarray[np.ndarray[float]] = find_quad_bezier_dnc(control_points1, i + 1, num_of_iter)
     bezier2: np.ndarray[np.ndarray[float]] = find_quad_bezier_dnc(control_points2, i + 1, num_of_iter)
+
+    # combine both result
+    return np.concatenate((bezier1, bezier2))
+
+
+# find midpoints for constructing general bezier curve
+def find_midpoints(
+        points: np.ndarray[np.ndarray[float]]
+        ) -> np.ndarray[np.ndarray[np.ndarray[float]]]:
+    
+    mid_points: np.ndarray[np.ndarray[float]] = np.empty((0, 2), dtype=np.float32)
+    for i in range(points.shape[0] - 1):
+        mid_point: np.ndarray[float] = np.array([(points[i] + points[i+1]) / 2])
+        mid_points = np.append(mid_points, mid_point, axis=0)
+    
+    # base case when there is only 1 midpoint result
+    if mid_points.shape[0] == 1:
+        return mid_points;
+    
+    mid_points2: np.ndarray[np.ndarray[float]] = find_midpoints(mid_points)
+    return np.concatenate((mid_points, mid_points2))
+
+
+# find points in general bezier curve with divide and conquer
+def find_bezier_dnc(
+        control_points: np.ndarray[np.ndarray[float]], 
+        i: int, 
+        num_of_iter: int
+        ) -> np.ndarray[np.ndarray[float]]:
+
+    mid_points: np.ndarray[np.ndarray[float]] = find_midpoints(control_points)
+    
+    # base case when we reach end of iteration
+    if i == num_of_iter:
+        return np.array((control_points[0], mid_points[-1], control_points[-1]))
+    
+    # find 1st half control points
+    control_points1: np.ndarray[np.ndarray[float]] = np.array([control_points[0]])
+    j: int
+    k: int = 1
+    for j in range(control_points.shape[0] - 1, 1, -1):
+        mid_point: np.ndarray[np.ndarray[float]] = np.array([mid_points[k - 1]])
+        control_points1 = np.append(control_points1, mid_point, axis=0)
+        k += j
+    
+    mid_point: np.ndarray[np.ndarray[float]] = np.array([mid_points[k - 1]])
+    control_points1 = np.append(control_points1, mid_point, axis=0)
+    
+    # find 2nd half control points
+    control_points2: np.ndarray[np.ndarray[float]] = np.array(mid_point)
+    for j in range(1, control_points.shape[0] - 1):
+        k -= j
+        mid_point: np.ndarray[np.ndarray[float]] = np.array([mid_points[k - 1]])
+        control_points2 = np.append(control_points2, mid_point, axis=0)
+    
+    control_points2 = np.append(control_points2, np.array([control_points[-1]]), axis=0)
+    
+    # find bezier curve points from the 1st half and 2nd half
+    bezier1: np.ndarray[np.ndarray[float]] = find_bezier_dnc(control_points1, i + 1, num_of_iter)
+    bezier2: np.ndarray[np.ndarray[float]] = find_bezier_dnc(control_points2, i + 1, num_of_iter)
+
+    # combine both result
     return np.concatenate((bezier1, bezier2))
--- src/main.py
+++ src/main.py
@@ -1,30 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-from bezier import find_quad_bezier_dnc
+from bezier import find_bezier_dnc
 
 
 # receive input from user
 
 # input number of points
-
 while True:
     try:
         num_of_points: int = int(input("Enter number of points: "))
-        if num_of_points > 0:
+        if num_of_points > 1:
             print()
             break
-        print("Number of points must be greater than 0.\n")
+        print("Number of points must be greater than 1.\n")
         
     except ValueError:
         print("Invalid input.\n")
 
 # input coordinates 
-
 input_x_temp: str
 input_y_temp: str
 input_points: np.ndarray[np.ndarray[float]] = np.empty((num_of_points, 2), dtype=np.float32)
-
 while True:
     print(f"Enter {num_of_points} points")
     try:
@@ -39,7 +36,6 @@ while True:
         print("Invalid input.\n")
 
 # input iteration
-
 while True:
     try:
         iteration: int = int(input("Enter iteration: "))
@@ -52,11 +48,11 @@ while True:
         print("Invalid input.\n")
 
 
-# finding points in bezier curve
+# find points in bezier curve
 
 start: float = time.time()
 
-bezier: np.ndarray[np.ndarray[float]] = find_quad_bezier_dnc(input_points, 1, iteration)
+bezier: np.ndarray[np.ndarray[float]] = find_bezier_dnc(input_points, 1, iteration)
 
 end: float = time.time()
 print(f"Execution time: {(end - start) * 1000} ms\n")
@@ -64,17 +60,13 @@ print(f"Execution time: {(end - start) * 1000} ms\n")
 
 # test plotting points and line
 
-input_xs = [point[0] for point in input_points]
-input_ys = [point[1] for point in input_points]
-bezier_xs = [point[0] for point in bezier]
-bezier_ys = [point[1] for point in bezier]
+input_xs, input_ys = input_points.transpose()
+bezier_xs, bezier_ys = bezier.transpose()
 
 fig, ax = plt.subplots()
 
 ax.scatter(input_xs, input_ys)
 ax.plot(input_xs, input_ys)
-for i, j in zip(input_xs, input_ys):
-    plt.text(i + 0.2, j + 0.2, f"({i}, {j})")
 
 ax.scatter(bezier_xs, bezier_ys)
 ax.plot(bezier_xs, bezier_ys)
